49dec0ac13e3664d3dc287c78affe869771f03e9
Ernandes R de Souza <ernandesrsouza@gmail.com>
2021-12-07 15:21:31 -0400

    Banco de dados com Migration(todos, daily_todos)

--- database/migrations/2021_12_07_190332_create_daily_todos_table.php
+++ database/migrations/2021_12_07_190332_create_daily_todos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateDailyTodosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('daily_todos', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->foreignId("users_id");
+            $table->foreignId("todos_id");
+            $table->foreign("todos_id")->references("id")->on("todos")->cascadeOnDelete();
+            $table->foreign("users_id")->references("id")->on("users")->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('daily_todos');
     }
 }
